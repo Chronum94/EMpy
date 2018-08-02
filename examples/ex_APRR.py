94102b3dbe267c597f60aefd2471f3e4f27c2080
Chronum <jinxhaldar@gmail.com>
2018-08-02 00:00:22 -0400

    Ring resonator example refactoring.

--- examples/ex_APRR.py
+++ examples/ex_APRR.py
@@ -1,17 +1,17 @@
 """All-pass ring resonator example."""
 
 import EMpy
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
-wls = numpy.linspace(1.5e-6, 1.6e-6, 1000)
-K = EMpy.devices.Coupler(wls, numpy.sqrt(0.08), 1.)
-l = 2 * numpy.pi * 5e-6
+wls = np.linspace(1.5e-6, 1.6e-6, 1000)
+K = EMpy.devices.Coupler(wls, np.sqrt(0.08), 1.)
+l = 2 * np.pi * 5e-6
 SWG = EMpy.devices.SWG(400, 220, 125).solve(wls)
 APRR = EMpy.devices.APRR(K, SWG.neff, l).solve()
 
-pylab.plot(wls, numpy.unwrap(numpy.angle(APRR.THRU)), "r.-")
-pylab.axis("tight")
-pylab.xlabel("wavelength /m")
-pylab.ylabel("phase")
-pylab.show()
+plt.plot(wls, np.unwrap(np.angle(APRR.THRU)), "r.-")
+plt.axis("tight")
+plt.xlabel("wavelength /m")
+plt.ylabel("phase")
+plt.show()
